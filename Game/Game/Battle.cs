3a501bfcb6ae3b891c3869f0c343c0fe42629ec3
NL547 <118132593+NL547@users.noreply.github.com>
2023-04-27 21:18:14 +0300

    Add files via upload

---
+++ Game/Game/Battle.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    static class Battle
+    {
+        public static int crit_chance = 16;
+        public static int attack_timer = 1500;
+
+        public static bool isCrit()
+        {
+            Random rnd = new Random();
+            int crit = rnd.Next(1, crit_chance);
+
+            if (crit == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool isPlayerDead(Player player) { return player.HP <= 0; }        
+
+        public static bool isEnemyDead(Enemy enemy) { return enemy.HP <= 0; }        
+
+        public static void PlayerAttack(Player player, Enemy enemy)
+        {
+            bool was_crit = false;
+
+            int DamageInflicted()
+            {
+                int damage = player.Attack() - enemy.Defense();
+                if (damage <= 1)
+                {
+                    damage = 1;
+                }
+                if (isCrit() == true)
+                {
+                    was_crit = true;
+                    damage += damage;
+                }
+                return damage;
+            }
+
+            enemy.HP -= DamageInflicted();
+
+            if (was_crit == true)
+            {
+                Console.WriteLine("Критический урон! Ты нанёс {0} урона.", DamageInflicted());
+            }
+            else
+            {
+                Console.WriteLine("Ты нанёс {0} урона.", DamageInflicted());
+            }
+            Console.WriteLine("У противника осталось {0} здоровья.", enemy.HP);
+        }
+
+        public static void EnemyAttack(Player player, Enemy enemy)
+        {
+            bool was_crit = false;
+
+            int DamageInflicted()
+            {
+                int damage = enemy.Attack() - player.Defense();
+                if (damage <= 0)
+                {
+                    damage = 0;
+                }
+                if (isCrit() == true)
+                {
+                    was_crit = true;
+                    damage += damage;
+                }
+                return damage;
+            }
+
+            player.HP -= DamageInflicted();
+
+            if (was_crit == true)
+            {
+                Console.WriteLine("Критический урон! Противник нанёс {0} урона.", DamageInflicted());
+            }
+            else
+            {
+                Console.WriteLine("Противник нанёс {0} урона.", DamageInflicted());
+            }
+            Console.WriteLine("У тебя осталось {0} здоровья.", player.HP);
+        }
+
+        public static void MakeBattle(Player player, Enemy enemy)
+        {
+            Console.WriteLine("Ты, {0}, сражаешься с {1} {2}.", player.name, enemy.specie, enemy.name);
+            enemy.Stats();
+            Console.WriteLine("Нажмите Enter чтобы начать сражение.");
+            Console.ReadLine();
+            Random rnd = new Random();
+            int move = rnd.Next(1, 3);
+            if (move == 1)
+            {
+                Console.WriteLine("Ты атакуешь первым!");
+                while (true)
+                {
+                    Console.WriteLine();
+                    System.Threading.Thread.Sleep(attack_timer);
+                    PlayerAttack(player, enemy);
+                    if (isEnemyDead(enemy))
+                    {
+                        Console.WriteLine("Ты убил {0}!", enemy.specie);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    }
+
+                    Console.WriteLine();
+                    System.Threading.Thread.Sleep(attack_timer);
+                    EnemyAttack(player, enemy);
+                    if (isPlayerDead(player))
+                    {
+                        Console.WriteLine("Герой пал! Игра окончена!");
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Противник атакует первым!");
+                while (true)
+                {
+                    Console.WriteLine();
+                    System.Threading.Thread.Sleep(attack_timer);
+                    EnemyAttack(player, enemy);
+                    if (isPlayerDead(player))
+                    {
+                        Console.WriteLine("Герой пал! Игра окончена!");
+                        break;
+                    }
+
+                    Console.WriteLine();
+                    System.Threading.Thread.Sleep(attack_timer);
+                    PlayerAttack(player, enemy);
+                    if (isEnemyDead(enemy))
+                    {
+                        Console.WriteLine("Ты убил {0}!", enemy.specie);
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Game/Game/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Enemy
+    {
+        public string specie;
+        public string name;
+        public int base_attack = 2;
+        public int base_defense = 1;
+        public int HP;
+
+        public int enemy_defense;
+        public int enemy_attack;
+
+        public Enemy(string aSpecie, string aName, int aHP, int aAttack, int aDefense)
+        {
+            specie = aSpecie;
+            name = aName;
+            HP = aHP;
+            enemy_attack = aAttack;
+            enemy_defense = aDefense;
+        }
+
+        public int Attack()
+        {
+            return (base_attack + enemy_attack);
+        }
+
+        public int Defense()
+        {
+            return (base_defense + enemy_defense);
+        }
+
+        public void Stats()
+        {
+            Console.WriteLine("{0} {1}:", specie, name);
+            Console.WriteLine("Здоровье: {0}", HP);
+            Console.WriteLine("Урон: {0}", Attack());
+            Console.WriteLine("Защита: {0}", Defense());
+        }
+    }
+}
